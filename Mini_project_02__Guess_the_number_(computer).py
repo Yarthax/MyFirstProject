f3faf398033147ed586f9137b8bd8a1cfaca8c7c
Yarthax <juancito.diego@gmail.com>
2022-06-20 13:45:02 -0300

    Este es el 1er intento simple sin funciones de armar el proyecto, solo con randint(), un while y if/elif/else. Comenté que necesita una condición para descartar input que no sean números, pero aún no sé como hacerlo.. Ahora veo de hacer un 2do intento con la ayuda del video https://youtu.be/8ext9G7xspg

--- Mini_project_02__Guess_the_number_(computer).py
+++ Mini_project_02__Guess_the_number_(computer).py
@@ -1,2 +1,23 @@
-print("Mini proyect 02: Guess the number game (computer),\
- Idea from https://www.freecodecamp.org/news/python-projects-for-beginners/")
+#Guess the number
+print("Mini proyect 02: Guess the number game (computer)\n\
+Idea from https://www.freecodecamp.org/news/python-projects-for-beginners/\n")
+
+
+#We'll be using the random library, hence we'll be importing it (see https://docs.python.org/3/library/random.html)
+import random
+#print(random.randint(1,20))   #Quick test
+
+random_number = random.randint (1,10)
+x = int(input ("Guess the number: "))
+continue_guessing = True
+# We need a condition for if the input is not a number at all
+while continue_guessing:
+    if random_number == x:
+        print (f"Well done!! The number is {x}")
+        continue_guessing = False
+    elif random_number > x:
+        x = int(input ("It is bigger than you think, can you guess? "))
+    else:
+        x = int(input ("It is smaller than you think, can you guess? "))
+
+print("Thank you for playing")
